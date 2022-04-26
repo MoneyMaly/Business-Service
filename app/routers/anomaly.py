5bc178a535328760e8ebe88d83c1c95ca47eef58
Tal Danilof <taldl@mta.ac.il>
2022-04-26 18:23:00 +0300

    Added Dates validation anomalyAPI

--- app/routers/anomaly.py
+++ app/routers/anomaly.py
@@ -1,8 +1,10 @@
 from typing import Optional, List
 import requests
 from fastapi import APIRouter, HTTPException, status, Depends
+from datetime import datetime
 
 from app.utils.auth_helper import JWTBearer
+from app.settings import BANK_API_URL
 
 router = APIRouter(tags=['Anomaly Detector'])
 
@@ -10,17 +12,27 @@ credentials_exception = HTTPException(
     status_code=status.HTTP_401_UNAUTHORIZED,
     detail="Could not validate credentials",
     headers={"WWW-Authenticate": "Bearer"})
+parameters_exception = HTTPException(
+    status_code=status.HTTP_400_BAD_REQUEST,
+    detail="Invalid Parameters",
+    headers={"WWW-Authenticate": "Bearer"})
 
 
 @router.get("/users/{username}/bankaccounts/balance/",status_code=status.HTTP_200_OK, dependencies=[Depends(JWTBearer())])
-async def get_monthly_balance_by_user(username: str):
+async def get_monthly_balance_by_user(username: str, from_year: int, from_month: int, to_year: int, to_month: int):
     if JWTBearer.authenticated_username != username:
         raise credentials_exception
     try:
-        res = requests.get(f"http://192.116.98.107:8082/users/{username}/bankaccounts/balance/?month=1&year=2021", 
-        headers={'Authorization':f'Bearer {JWTBearer.jwtoken}'})
-    # bank_accounts_list = await get_bank_accounts_list_by_username(username)
-    # monthly_balance = await get_monthly_balance(bank_accounts_list, year, month)
-    except:
-        raise Exception
+        from_date = datetime(from_year, from_month, 1)
+        to_date = datetime(to_year, to_month, 1)
+        if from_date < to_date:
+            try:
+                res = requests.get(f"{BANK_API_URL}/users/{username}/bankaccounts/balance/?month=1&year=2021", 
+                headers={'Authorization':f'Bearer {JWTBearer.jwtoken}'})
+            except Exception as e:
+                raise 
+        else:
+            return parameters_exception
+    except Exception as e:
+        raise parameters_exception
     return ["tal communications", "Hot TV"]
